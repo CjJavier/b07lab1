feef08e572c6bc65ec7eb1c28d5ef43c1318b5fd
User <user@Users-MacBook-Pro.local>
2024-09-29 23:28:32 -0400

    modified

--- Polynomial.java
+++ Polynomial.java
@@ -2,42 +2,69 @@ import java.lang.Math;
 
 public class Polynomial {
 	// fields
-	double [] coefficients;
+	double [] coefficients; // must be non-zero coefficients
+	int [] exponents;
 
 	// methods
 	public Polynomial() {
 		coefficients = new double[] {0.0};
+		exponents = new int[] {0};
 	}
 
-	public Polynomial(double [] a) {
-		int len = a.length;
-		coefficients = new double[len];
-		for (int i = 0; i < len; i++) {
+	// a is an array of coefficients
+	// b is an array of exponents
+	// a and b both have the same length
+	public Polynomial(double [] a, int [] b) {
+		int len1 = a.length;
+		int len2 = b.length;
+		coefficients = new double[len1];
+		exponents = new int [len2];
+		for (int i = 0; i < len1; i++) {
 			coefficients[i] = a[i];
+			exponents[i] = b[i];
 		}
 	}
+/*
+	public int search_exp(int [] exp, int a) {
+		for (int i = 0; i < exp.length; i++) {
+			if (exp[i] == a)
+				return true;
+		}
+		return false;
+	}
+*/
+/*
+	// method to get the length of the result polynomial after adding two polynomials
+	public int get_length(Polynomial poly) {
 
+		int result = 0;
+		// for every distinct exponent we find, add it to result
+		for (int i = 0; i)
+
+	}
+*/
 	public Polynomial add(Polynomial poly) {
 	
 	    int len1 = coefficients.length;
 	    int len2 = poly.coefficients.length;
+
 	
-	    double[] result;
-	
+	    double [] result_c;
+	    int [] result_e;
+	    int counter = 0;
+
 	    if (len1 > len2) {
-	    	result = new double[len1];
+	    	result_c = new double[len1];
+	    	result_e = new int[len1];
+
 	    } else {
-	    	result = new double[len2];
+	    	result_c = new double[len2];
+	    	result_e = new int[len2];
 	    }
+
+	    
 	
-	    for (int i = 0; i < coefficients.length; i++) {
-	        result[i] += coefficients[i];
-	    }
-	    for (int i = 0; i < poly.coefficients.length; i++) {
-	        result[i] += poly.coefficients[i];
-	    }
-	
-	    return new Polynomial(result);
+	    return new Polynomial(result_c, result_e);
 	}
 
 
@@ -53,4 +80,28 @@ public class Polynomial {
 	public boolean hasRoot (double x) {
 		return evaluate(x) == 0;
 	}
+
+/*
+	public Polynomial multiply (Polynomial poly) {
+		
+		// nested loop
+		// for each term in the calling object
+			// multiply the coefficient
+
+		int len1 = coefficients.length;
+		int len2 = poly.coefficients.length;
+
+
+
+	} */
 }
+
+
+
+
+
+
+
+
+
+
